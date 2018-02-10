e48e59ec8d78743dbf3858bc42ef67e62695db46
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-09 14:39:59 -1000

    Add files via upload

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var Genre = new Schema({
+  name: [{type: String, min: 3, max: 100}]
+});
+
+// Virtual for genre's URL
+Genre
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', Genre);
